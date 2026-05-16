2418350c608f1452521b909c2e92b17f22b61356
franciscopinedamarquez-web <franciscopinedamarquez@gmail.com>
2026-05-16 20:04:53 +0000

    fix: limpiar conflictos de merge en archivos app/

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-// app/index.tsx
 import HomeScreen from '../src/screens/HomeScreen';
 export default HomeScreen;
-=======
-import HomeScreen from '../src/screens/HomeScreen';
-export default HomeScreen;
->>>>>>> 2acf2a7f9bc79f41b58a0a608d9f535e0428a43f
--- app/search.tsx
+++ app/search.tsx
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-// app/search.tsx
 import SearchScreen from '../src/screens/SearchScreen';
 export default SearchScreen;
-=======
-import SearchScreen from '../src/screens/SearchScreen';
-export default SearchScreen;
->>>>>>> 2acf2a7f9bc79f41b58a0a608d9f535e0428a43f
